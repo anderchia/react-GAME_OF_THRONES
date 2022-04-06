1b2a81ec4f06ab05c0790d05dbbb8c8e1cdddbb2
BellaMir <bellacontrerascamperos@yahoo.es>
2022-04-06 18:08:01 +0200

    Home

---
+++ Desktop/GAME OF THRONES/game_of_thrones/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <>
+     <div className="p-homeTitle">
+      <h1 className="p-h1Title">GAME OF THRONES</h1>
+     </div> 
+    </>
+  );
+}
