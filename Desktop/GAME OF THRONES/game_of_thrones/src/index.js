21674dd368959ed86760b412f705b5d8da77861b
Ander <anderazp@gmail.com>
2022-04-09 18:15:38 +0200

    translator

--- Desktop/GAME OF THRONES/game_of_thrones/src/index.js
+++ Desktop/GAME OF THRONES/game_of_thrones/src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import {IntlProvider} from 'react-intl';
+import Spanish from './lang/es.json';
+import English from './lang/en.json';
+
+const locale = navigator.language;
+let lang;
+if (locale==="es") {
+   lang = Spanish;
+} else {
+     if (locale === "en"){  
+       lang = English;
+     }
+}
 
 ReactDOM.render(
+   <IntlProvider locale ={locale} messages={Spanish}>
+       <App />
+   </IntlProvider>,
+   document.getElementById('root')
+);
+serviceWorker.unregister();
+
+
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
