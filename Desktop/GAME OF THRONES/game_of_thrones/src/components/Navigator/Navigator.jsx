f373c0ebf2729d1ea9805d3b9774cba85bf81d6c
BellaMir <bellacontrerascamperos@yahoo.es>
2022-04-09 18:04:55 +0200

    change translator

--- Desktop/GAME OF THRONES/game_of_thrones/src/components/Navigator/Navigator.jsx
+++ Desktop/GAME OF THRONES/game_of_thrones/src/components/Navigator/Navigator.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import './Navigator.scss'
+import './Navigator.scss';
+import { useTranslation } from 'react-i18next';
+
+const lngs = {
+  es: { nativeName: 'Spanish' },
+  en: { nativeName: 'English' }
+};
 
 export default function Navigator() {
+  const { t, i18n } = useTranslation();
   return (
     <>
+        <div className='b-buttons'>
+         
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} className="b-buttonNav" style={{ backgroundImage:  i18n.resolvedLanguage === lng ? '../../../public/images/spanish.jpg' : '../../../public/images/Bandera-de-Reino-Unido.png' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </button>
+            
+          ))}
+          </div>
+       
+        
      <ul className='c-nav'>
-        <Link to ="/characters">PERSONAJES</Link> 
-        <Link to ="/houses">CASAS</Link> 
-        <Link to ="/cronology">CRONOLOGIA</Link> 
+        <Link to ="/characters">{t('description.part1')}
+        </Link> 
+        <Link to ="/houses">
+        {t('description.part2')}</Link> 
+        <Link to ="/cronology">{t('description.part3')}</Link> 
      </ul>
-    </>
+   </> 
   )
 }
 
