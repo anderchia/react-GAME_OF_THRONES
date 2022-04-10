3f5ed6ea2b0a3e78a63e65db3d707280db316f5d
Ander <anderazp@gmail.com>
2022-04-10 12:01:44 +0200

    cronology2

--- Desktop/GAME OF THRONES/game_of_thrones/src/pages/Cronology/Cronology.jsx
+++ Desktop/GAME OF THRONES/game_of_thrones/src/pages/Cronology/Cronology.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './Cronology.scss'
+import Cronologyasc from '../../components/Cronologies/Cronologyasc'
+import Cronologydesc from '../../components/Cronologies/Cronologydesc'
+
 
 
 
 export default function Cronology() {
     const [characters, setCharacters] = useState([])
- 
+  
     const getCharacters = async () => {
         const res= await axios.get(`https://api.got.show/api/show/characters/`);
       
@@ -14,21 +18,45 @@ export default function Cronology() {
       
       }
 
+  
     useEffect(() => {
       getCharacters();
     }, [])
   
     console.log(characters) 
-  
-  
+
+        // document.getElementById('asc').onclick = () => {
+        //   const ascList = characters.filter(character=> character.age.age).sort((prev, next) => {
+        //     return prev.age.age - next.age.age;
+        //   });
+          
+        //   characters(ascList);
+        // }
+   
+        const [showCronology, setShowCronology] = useState(true);
+       
+
       return (
         <div>
-        <ul className='b-li'>
+        
+        <button id="asc" onClick={() => setShowCronology(true)}>Ascending</button>
+        <button id="asc" onClick={() => setShowCronology(false)}>Descending</button>
+        {showCronology ? (
+          <Cronologyasc characters={characters}/>
+        ) : (
+          <Cronologydesc characters={characters}/>
+        )}
+      
+        <ul>
           {characters.filter(character => character.age && character.age.age)
             .sort((prev, next) => prev.age.age - next.age.age)
             .map(character => (
               <li key={character.id}>
-                {character.image && <img src={character.image} alt="personajes" ></img>} {character.age.name} {character.age.age}
+             
+              <p>{character.age.age}</p>
+              <p>{character.age.name}</p>
+                 {character.image && <img src={character.image} alt="personajes" ></img>} 
+          
               </li>
             ))}
         </ul>
