0e3719721fdc7e6584e0ba960ca9451cf654e521
BellaMir <bellacontrerascamperos@yahoo.es>
2022-04-10 10:23:42 +0200

    cronology

--- Desktop/GAME OF THRONES/game_of_thrones/src/pages/Cronology/Cronology.jsx
+++ Desktop/GAME OF THRONES/game_of_thrones/src/pages/Cronology/Cronology.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function Cronology() {
-  return (
-    <div>Cronology</div>
-  )
-}
 
-export default Cronology
+
+export default function Cronology() {
+    const [characters, setCharacters] = useState([])
+ 
+    const getCharacters = async () => {
+        const res= await axios.get(`https://api.got.show/api/show/characters/`);
+      
+        console.log(res.data);
+        setCharacters(res.data)
+      
+      }
+
+    useEffect(() => {
+      getCharacters();
+    }, [])
+  
+    console.log(characters) 
+  
+  
+      return (
+        <div>
+        <ul className='b-li'>
+          {characters.filter(character => character.age && character.age.age)
+            .sort((prev, next) => prev.age.age - next.age.age)
+            .map(character => (
+              <li key={character.id}>
+                {character.image && <img src={character.image} alt="personajes" ></img>} {character.age.name} {character.age.age}
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
+  }
